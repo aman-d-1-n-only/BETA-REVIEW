4cfa24554bc6d2e8215b97cdf363e4fa5d54484d
aman-d-1-n-only <abhitheone456@gmail.com>
2020-06-20 09:12:07 +0530

    Set-Up of Comment Edit Route

--- routes/comment.js
+++ routes/comment.js
@@ -42,7 +42,18 @@ router.post('/', isLoggedIn, function(req, res) {
     })
 })
 
-//middleware
+//comment edit route
+router.get("/:comment_id/edit", function(req, res) {
+        Comment.findById(req.params.comment_id, function(err, foundComment) {
+            if (err) {
+                res.redirect("back");
+            } else {
+                res.render('Comments/edit', { MovieInfo_id: req.params.id, comment: foundComment });
+            }
+        });
+
+    })
+    //middleware
 function isLoggedIn(req, res, next) {
     if (req.isAuthenticated()) {
         return next();
