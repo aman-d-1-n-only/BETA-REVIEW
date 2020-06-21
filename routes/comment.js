e820db6a4d97e7b85fccfdce00348b8d4c182ac2
aman-d-1-n-only <abhitheone456@gmail.com>
2020-06-21 23:38:07 +0530

    Set-up of delete route for comment

--- routes/comment.js
+++ routes/comment.js
@@ -65,6 +65,17 @@ router.put("/:comment_id", function(req, res) {
     })
 })
 
+//COMMENT DESTROY ROUTE
+router.delete("/:comment_id", function(req, res) {
+    Comment.findByIdAndRemove(req.params.comment_id, function(err) {
+        if (err) {
+            res.redirect('back');
+        } else {
+            res.redirect('/moviereviews/' + req.params.id);
+        }
+    })
+})
+
 //middleware
 function isLoggedIn(req, res, next) {
     if (req.isAuthenticated()) {
