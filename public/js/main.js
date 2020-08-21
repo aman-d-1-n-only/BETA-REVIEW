40c66499f93dd8171055f2e74381f9556ae58020
aman-d-1-n-only <abhitheone456@gmail.com>
2020-08-21 22:51:34 +0530

    Main Page Set Up

--- public/js/main.js
+++ public/js/main.js
@@ -3,3 +3,22 @@ $('#menu-toggle').click(() => {
     $('.ui.sidebar')
         .sidebar('toggle');
 });
+
+/*Set up of owl carousel */
+$('.owl-carousel').owlCarousel({
+    stagePadding: 50,
+    loop: true,
+    margin: 10,
+    nav: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 3
+        },
+        1000: {
+            items: 5
+        }
+    }
+});
